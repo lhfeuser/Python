6c00c0bbc11f785fa859e0dd4227ac40915bcfee
Lucas Feuser <90938659+lhfeuser@users.noreply.github.com>
2023-10-21 19:18:17 -0300

    primeira_solução

---
+++ Aula 16/aula16ds72.py
@@ -0,0 +1,5 @@
+escolha = int(input('Entre com o valor que você quer ver escrito por extenso \nNúmero: '))
+
+extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+print(f'O número que você escolheu escrito por extenso é {extenso[escolha]}')
