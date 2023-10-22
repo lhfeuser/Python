96cf402c00cb4fc3c19da6eafee31c44252f1503
Lucas Feuser <90938659+lhfeuser@users.noreply.github.com>
2023-10-21 22:22:25 -0300

    verifica_se_entrou_com_num_certo

--- Aula 16/aula16ds72.py
+++ Aula 16/aula16ds72.py
@@ -1,5 +1,8 @@
-escolha = int(input('Entre com o valor que você quer ver escrito por extenso \nNúmero: '))
-
 extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
+while True:
+    escolha = int(input('Qual número você quer ver escrito por extenso? \nNúmero: '))
+    if 0 <= escolha <= 20:
+        break
+    
 print(f'O número que você escolheu escrito por extenso é {extenso[escolha]}')
